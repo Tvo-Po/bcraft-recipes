c3e226a5ae79aa92b45e6ab5a5b3534e345be5b1
Tvo-Po <maxwellgleb@gmail.com>
2023-04-25 21:59:15 +0300

    Set up DB
    Create settings for application.
    Main tools for db handling.
    Create Database model schema for recipes.
    Set up alembic migrations.

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from pydantic import BaseSettings
+
+
+project_path = Path(__file__).parent.parent
+
+
+class Settings(BaseSettings):
+    PROJECT_PATH: Path = project_path
+
+    DEBUG: bool = True
+
+    DATABASE_URL: str
+
+    class Config:
+        env_file = project_path / '.env'
+
+
+settings = Settings()  # type: ignore
---
+++ app/model.py
@@ -0,0 +1,68 @@
+from sqlalchemy import Index, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .db import Base
+
+
+class Recipe(Base):
+    __tablename__ = 'recipe'
+
+    id: Mapped[int] = mapped_column(
+        autoincrement=True,
+        primary_key=True,
+        index=True,
+    )
+    name: Mapped[str] = mapped_column(String(127), nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=False)
+    ingredients: Mapped[list['RecipeIngredientAssociation']] = relationship(
+        cascade='all, delete-orphan',
+    )
+    steps: Mapped[list['Step']] = relationship(
+        back_populates='recipe',
+        cascade='all, delete-orphan',
+    )
+
+
+class RecipeIngredientAssociation(Base):
+    __tablename__ = 'recipe_ingredient_association'
+    __table_args__ = (
+        Index(
+            'recipe_ingredient_association_composite_pk',
+            'recipe_id',
+            'ingredient_id',
+        ),
+    )
+    
+    recipe_id: Mapped[int] = mapped_column(
+        ForeignKey('recipe.id'),
+        primary_key=True,
+    )
+    ingredient_id: Mapped[int] = mapped_column(
+        ForeignKey('ingredient.id', ondelete='RESTRICT'),
+        primary_key=True,
+    )
+    ingredient: Mapped['Ingredient'] = relationship()
+
+
+class Ingredient(Base):
+    __tablename__ = 'ingredient'
+    
+    id: Mapped[int] = mapped_column(
+        autoincrement=True,
+        primary_key=True,
+        index=True,
+    )
+    name: Mapped[str] = mapped_column(String(127), nullable=False)
+
+
+class Step(Base):
+    __tablename__ = 'step'
+    __table_args__ = (Index('step_composite_pk', 'recipe_id', 'order'), )
+    
+    recipe_id: Mapped[int] = mapped_column(
+        ForeignKey("recipe.id"),
+        primary_key=True,
+    )
+    recipe: Mapped['Recipe'] = relationship(back_populates="steps")
+    order: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(String(255), nullable=False)
